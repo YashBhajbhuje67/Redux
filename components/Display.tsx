d0e445368e4e18e251fc22345e28a9024a9af6bf
YashBhajbhuje67 <yashbhajbhuje67@gmail.com>
2023-08-27 11:44:24 +0530

    added required component and added navigation screens

---
+++ components/Display.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View } from 'react-native'
+
+const Display = () => {
+  return (
+    <View>
+      
+    </View>  
+  )
+}
+
+export default Display
---
+++ components/Form.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View } from 'react-native'
+
+const Form = () => {
+  return (
+    <View>
+      
+    </View>
+  )
+  }
+
+export default Form
